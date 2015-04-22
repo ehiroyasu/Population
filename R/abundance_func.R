ce9ebe448737a654646329bf1855d6569765b5c9
Bruce Kendall <kendall@bren.ucsb.edu>
2015-04-22 00:50:23 -0700

    Minor changes to documentation

--- R/abundance_func.R
+++ R/abundance_func.R
@@ -1,5 +1,3 @@
-#'abundance_func
-#'
 #'Calculating abundances with adjusted survivals
 #'This function uses matrix multiplication to calculate abundances from
 #'transition matrices and N0 data
@@ -11,7 +9,7 @@
 #'Demographic Research (Germany). Available at www.compadre-db.org 
 #'(data downloaded on [1/1/2014]).
 #'@references Martha M. Ellis, Jennifer L. Williams, Peter Lesica, Timothy J. Bell, Paulette Bierzychudek, Marlin Bowles, 
-#'  Elizabeth E. Crone, Daniel F. Doak, Johan Ehrlén, Albertine Ellis-Adam, Kathryn McEachern, Rengaian Ganesan, 
+#'  Elizabeth E. Crone, Daniel F. Doak, Johan Ehrl?n, Albertine Ellis-Adam, Kathryn McEachern, Rengaian Ganesan, 
 #'  Penelope Latham, Sheila Luijten, Thomas N. Kaye, Tiffany M. Knight, Eric S. Menges, William F. Morris, 
 #'  Hans den Nijs, Gerard Oostermeijer, Pedro F. Quintana-Ascencio, J. Stephen Shelly, Amanda Stanley, Andrea Thorpe, 
 #'  Tamara Ticktin, Teresa Valverde, and Carl Weekley. 2012. Matrix population models from 20 studies of perennial 
