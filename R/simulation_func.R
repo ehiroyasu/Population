758dc3cb500c441330a5f1c7ad2efab98893ecf6
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-05-19 23:19:20 -0700

    adjusted simulation to insert trend in stage 4 for exercise

--- R/simulation_func.R
+++ R/simulation_func.R
@@ -23,17 +23,17 @@ trend_sim<-function(surv_rand, fert_rand, N0_rand, nstage, surv_trend){
   nstage<-1:dim(surv_rand)[1]
   
   ##to modify stage 2 survival, use trend function
-  surv_trend <- insert_survival_trend(surv_rand, beta = 0.1, nstage, stage2mod=2)
+  surv_trend <- insert_survival_trend(surv_rand, beta = 0.1, nstage, stage2mod=4)
   
   ##calculating new abundance matrices
   abundance<- abundance_func(N0_rand, surv_trend, fert_rand)
   
   ##survival regression
-  lm_surv<- surv_regr(surv_trend, years, stage2mod=2)
+  lm_surv<- surv_regr(surv_trend, years, stage2mod=4)
   pv_surv<-(lm_surv$coefficients[2,4])
   
   ##abundance regression
-  lm_abundance<- abundance_regr(abundance, years, stage2mod=2)
+  lm_abundance<- abundance_regr(abundance, years, stage2mod=4)
   pv_abundance<- (lm_abundance$coefficients[2,4])
   
   return(list("survival p-value"=pv_surv, "abundance p-value"=pv_abundance))
