5d3ce2f88b3b0916c15a8cea874b8f4880eb553c
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-05-19 23:22:43 -0700

    adjusted abundance function to reflect cleaner years definition

--- R/abundance_func.R
+++ R/abundance_func.R
@@ -20,9 +20,9 @@
 
 abundance_func<- function(N0_rand, surv_trend, fert_rand){
   trans_mat<- surv_trend + fert_rand
-  abundance <- matrix(nrow=length(years)+1, ncol=nrow(surv_trend))
+  abundance <- matrix(nrow=length(years)+1, ncol=(dim(surv_trend)[1]))
   abundance[1,] <- N0_rand[1,]
-  for (i in 1:years){
+  for (i in years){
     #abundance<-N0_rand
     abundance[i+1,]<-trans_mat[,,i]%*%abundance[i,]
   }
