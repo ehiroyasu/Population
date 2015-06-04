dd22fe3b3c375b9539d8b5cee49318f321695345
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-06-04 13:42:48 -0700

    created a new function to extract survival, fertility, and transition matrices from Compadre data.

---
+++ R/extract_mat.R
@@ -0,0 +1,29 @@
+#'extract_mat
+#'
+#'Extracts survival matrices from  
+#'
+#'@param MatrixData matrix population data from the COMPADRE matrix data
+#'
+#'@references COMPADRE Plant Matrix Database. Max Planck Institute for Demographic Research (Germany). Available at www.compadre-db.org (data downloaded on [1/1/2014]).
+#'@author Elizabeth Hiroyasu
+#'
+
+extract_mat<- function(MatrixData){
+  #extracting survival matrices
+  surv_mat<-array(1, dim=c(dim(MatrixData[[1]]$matU)[1], dim(MatrixData[[1]]$matU)[2], dim(MatrixData)))
+  for (i in 1:dim(MatrixData)){
+    surv_mat[,,i]<-MatrixData[[i]]$matU
+  }
+  ##pulling out LoBr reproduction
+  fert_mat<-array(1, dim=c(dim(MatrixData[[1]]$matF)[1], dim(MatrixData[[1]]$matF)[2], dim(MatrixData)))
+  for (i in 1:dim(MatrixData)){
+    fert_mat[,,i]<-MatrixData[[i]]$matF
+  }
+  
+  ##pulling out LoBr transition matrices
+  trans_mat<-array(1, dim=c(dim(MatrixData[[1]]$matA)[1], dim(MatrixData[[1]]$matA)[2], dim(MatrixData)))
+  for (i in 1:dim(MatrixData)){
+    trans_mat[,,i]<-MatrixData[[i]]$matA
+  }
+  return(list("survival matrices"=surv_mat, "fertility matrices"=fert_mat, "transition matrices"=trans_mat))
+}
