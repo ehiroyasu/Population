df4be9cb0044c1982d41488ce7b2355e441a38d3
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-06-04 10:54:34 -0700

    renamed "abundance_func" to "calc_abundance"

--- R/calc_abundance.R
+++ R/calc_abundance.R
@@ -18,7 +18,7 @@
 #'@author Elizabeth Hiroyasu
 
 
-abundance_func<- function(N0_rand, surv_trend, fert_rand){
+calc_abundance<- function(N0_rand, surv_trend, fert_rand){
   trans_mat<- surv_trend + fert_rand
   abundance <- matrix(nrow=length(years)+1, ncol=(dim(surv_trend)[1]))
   abundance[1,] <- N0_rand[1,]
