8b389c0141e1d6d06c9732f04b77f0651cab7118
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-06-10 10:44:15 -0700

    created new function to calculate how often survival p-value is less than alpha

---
+++ R/surv_lessthan_alpha.R
@@ -0,0 +1,23 @@
+#'surv_lessthan_alpha
+#'
+#'counting the number of times the p-value for survival and the p-value for abundance are les than alpha, this will 
+#'allow us to assess power.
+#'
+#'
+#'@param survival_pv is a vector of p-values for the regression of survival over time
+#'@param alpha is a vector of alpha values over which to test if survival is less than
+#'
+#'@return demog a vector of counts of the number of times the survival p-value is less than the alpha value
+#'
+#'@references data derived from COMPADRE Plant Matrix Database. Max Planck Institute for Demographic Research (Germany). Available at www.compadre-db.org (data downloaded on [1/1/2014]).
+#'@author Elizabeth Hiroyasu
+
+surv_lessthan_alpha<- function(survival_pv, alpha){
+
+  demog<-vector(length=length(alpha))
+   
+  for (a in 1:length(alpha)){
+      demog[a]<-sum(survival_pv<alpha[a])
+    }
+  return(demog)
+  }
