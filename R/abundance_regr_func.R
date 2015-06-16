f9fa2f44e62985c9f4eca833c003b514da14ce63
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-06-16 15:56:16 -0700

    added test/warning for NA p-values

--- R/abundance_regr_func.R
+++ R/abundance_regr_func.R
@@ -15,5 +15,10 @@ abundance_regr<- function(abundance, years, active_stages){
   tot_abundance <- apply(active_abundance, 1, sum)
 #  lm_abundance<- summary(lm(stage_abundance~poly(years,stage2mod)))
   lm_abundance <- summary(lm(tot_abundance ~ poly(years, 2)))
+
+  if (lm_abundance$coefficients[3,4]=='NaN'){
+    warning('p-value=NaN')
+  }
+
   return(lm_abundance)
 }
