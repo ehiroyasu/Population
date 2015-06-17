b71dbbc6dff134022cb9f3fda39e70bbe49e478d
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-06-17 16:12:58 -0700

    generalized pv_less than alpha from three functions to one

--- R/pv_lessthan_alpha.R
+++ R/pv_lessthan_alpha.R
@@ -1,9 +1,9 @@
-#'abundance_lessthan_alpha
+#'pv_lessthan_alpha
 #'
 #'counting the number of times p-value for abundance is less than alpha, this will 
 #'allow us to assess power.
 #'
-#'@param abundance_pv is a vector of p-values for the regression of abundance over time
+#'@param pvalue is a vector of p-values 
 #'@param alpha is a vector of alpha values over which to test if abundance is less than
 #'
 #'@return prop_N a vector of proportional counts of the number of times the abundance p-value is less than the alpha value
@@ -11,13 +11,13 @@
 #'@references data derived from COMPADRE Plant Matrix Database. Max Planck Institute for Demographic Research (Germany). Available at www.compadre-db.org (data downloaded on [1/1/2014]).
 #'@author Elizabeth Hiroyasu
 
-abundance_lessthan_alpha<- function(abundance_pv, alpha){
+pv_lessthan_alpha<- function(pvalue, alpha){
   
-  prop_N<-vector(length=length(alpha))
+  prop<-vector(length=length(alpha))
   
   for (a in 1:length(alpha)){
-    prop_N[a]<-(sum(abundance_pv<alpha[a]))/length(alpha)
+    prop[a]<-(sum(pvalue<alpha[a]))/length(pvalue)
   }
-  return(prop_N)
+  return(prop)
 }
 
