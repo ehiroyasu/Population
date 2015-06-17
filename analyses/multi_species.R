1ccc9cdd8c0e56fb3fa4d3cd63018a6e52679ada
Bruce Kendall <kendall@bren.ucsb.edu>
2015-06-17 15:37:01 -0700

    Code for looping through unique populations

--- analyses/multi_species.R
+++ analyses/multi_species.R
@@ -11,4 +11,18 @@ for (k in length(species)){
 }
 
 
+pop_list <- unique(cbind(mydata$metadata$SpeciesAuthor,mydata$metadata$Population))
+num_pops <- dim(pop_list)[1]
+num_pops <- 2 # for debugging
 
+for (k in 1:num_pops) {
+  temp<-subset(mydata$metadata, SpeciesAuthor==pop_list[k,1] & Population==pop_list[k,2])
+  
+  #extracting CiPi Matrices
+  save<-as.numeric(rownames(temp))
+  MatrixData<- as.array(mydata$mat[mydata$metadata$SpeciesAuthor==pop_list[k,1] & mydata$metadata$Population==pop_list[k,2]])
+  MatClass<-mydata$mat_class[save]
+  print(pop_list[k,])
+  
+  # Insert rest of analysis
+}
