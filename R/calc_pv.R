b6e2419425622f4a30b07913768c99dd9ea7f30d
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-06-19 17:33:05 -0700

    adjusted calc_pv function to reflect debug changes

--- R/calc_pv.R
+++ R/calc_pv.R
@@ -14,26 +14,22 @@
 #'
 
 
-calc_pv<-function(surv_mat, fert_mat, N0_data, nstage, years, stage2mod, beta, active_stages, verbose=FALSE){
+calc_pv<-function(surv_mat, fert_mat, trans_mat, N0_data, nstage, years, stage2mod, beta, active_stages, verbose=FALSE){
   #sampling random survival matrices and fertility matrices with n years in simulation
   i_rand <- sample(1:dim(surv_mat)[3], size=length(years), replace=T)
   surv_rand <- surv_mat[,,i_rand]
   fert_rand <- fert_mat[,,i_rand]
-  
-  trans_mat <- surv_mat+fert_mat
-  N0_data<-gen_N0(trans_mat)
-  
-  #calculating the variance in survival:
-  #surv_mad<- calc_surv_mad(surv_rand, stage2mod, years)
-   
-  ##to modify stage 2 survival, use trend function
+
+
+  ##inserting a trend into survival
   temp <- insert_survival_trend(surv_rand, beta, nstage, stage2mod, years, verbose)
   surv_trend <- temp[[1]]
   ##to print the number of times the survival goes to zero in the stages where a trend has been inserted
   surv_zero <- temp[[2]]
+  surv_na<-temp[[3]]
   
   ##calculating new abundance matrices
-  abundance<- calc_abundance(N0_data, surv_trend, fert_rand, years)
+  abundance<- calc_abundance(N0_data, surv_trend, fert_rand, trans_mat, years)
   if (verbose) {
     print(abundance)
     plot(abundance[,active_stages])
@@ -43,9 +39,7 @@ calc_pv<-function(surv_mat, fert_mat, N0_data, nstage, years, stage2mod, beta, a
   lm_surv<- surv_regr(surv_trend, active_stages)
   pv_surv<-(lm_surv$coefficients[2,4])
   
-  #calculating survival variance
-  surv_var<-var(resid(lm_surv))
-  
+
   ##abundance regression
   lm_abundance<- abundance_regr(abundance[-1,], years, active_stages)
   pv_abundance<- (lm_abundance$coefficients[3,4])
@@ -65,9 +59,13 @@ calc_pv<-function(surv_mat, fert_mat, N0_data, nstage, years, stage2mod, beta, a
   lm_lambda<-lambda_regr(lambda, years)
   pv_lambda<-(lm_lambda$coefficients[2,4])
   
-  #lambda regression variance
+  #calculating variances
+  surv_var<-var(resid(lm_surv))
+  abundance_var<-var(resid(lm_abundance))
   lambda_var<-var(resid(lm_lambda))
   
+  
   return(list("survival p-value"=pv_surv, "abundance p-value"=pv_abundance, "lambda p-value" = pv_lambda, 
-              "n surv=0"=surv_zero, "survival variance"=surv_var, "lambda variance"=lambda_var))
+              "n surv=0"=surv_zero, "n surv=na"=surv_na, "survival variance"=surv_var, "abundance variance" = abundance_var, 
+              "lambda variance"=lambda_var))
 }
