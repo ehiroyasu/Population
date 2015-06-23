1410b058bb48f9f18778e17c88db5d2b606066a7
ehiroyasu <ehiroyasu@bren.ucsb.edu>
2015-06-22 23:15:11 -0700

    added multi species analysis. NOTE I'm not positive this works at the moment as I was having some serious software issues tonight, will check first thing in the morning.

---
+++ R/analyze_pv.R
@@ -0,0 +1,40 @@
+#'analyze_pv
+#'
+#'Analyzes the output p-values from the calc_pv function. This function can be used to analyze multiple
+#'species at a time.
+#'
+#'@param alpha is a vector of alpha values to compare p-values to and to assess power. Default alpha values are alpha=seq(from=0.01, to=0.2, by=delta) 
+#'@param return_pv is list with p-values returned from regression analysis
+#'
+#'@references data derived from COMPADRE Plant Matrix Database. Max Planck Institute for Demographic Research (Germany). Available at www.compadre-db.org (data downloaded on [1/1/2014]).
+#'@author Elizabeth Hiroyasu
+
+analyze_pv<-function(alpha, return_pv){
+  
+  survival_pv<-unlist(return_pv[1,])
+  abundance_pv<-unlist(return_pv[2,])
+  lambda_pv<-unlist(return_pv[3,])
+  
+  prop_demog<-pv_lessthan_alpha(pvalue=survival_pv, alpha)
+  prop_N<-pv_lessthan_alpha(pvalue=abundance_pv,alpha)
+  prop_lambda<-pv_lessthan_alpha(pvalue=lambda_pv, alpha)
+
+  prop_demog_0.05<-pv_lessthan_alpha(pvalue=survival_pv, alpha=0.05)
+  prop_lambda_0.05<-pv_lessthan_alpha(pvalue=lambda_pv, alpha=0.05)
+  
+  return(list(survival_pv, abundance_pv, lambda_pv, prop_demog, prop_N, prop_lambda, prop_demog_0.05, prop_lambda_0.05))
+}
+
+
+
+
+
+survival_pv<-unlist(return_pv[1,])
+abundance_pv<-unlist(return_pv[2,])
+lambda_pv<-unlist(return_pv[3,])
+
+prop_demog<-pv_lessthan_alpha(pvalue=survival_pv, alpha)
+prop_N<-pv_lessthan_alpha(pvalue=abundance_pv,alpha)
+prop_lambda<-pv_lessthan_alpha(pvalue=lambda_pv, alpha)
+
+diff<-(prop_lambda-prop_demog)
--- analyses/multi_species.R
+++ analyses/multi_species.R
@@ -19,47 +19,37 @@ for (k in 1:num_pops) {
 
   active_stages<-subset(MatClass[[1]]$MatrixClassNumber, MatClass[[1]]$MatrixClassOrganized=='active')
 
-  ##pulling out survival matrices
-  surv_mat<-(extract_mat(MatrixData))$"survival matrices"
-  
-  ##pulling out fertility matrices
-  fert_mat<-(extract_mat(MatrixData))$"fertility matrices"
-  
-  ##pulling out transition matrices
-  trans_mat<-(extract_mat(MatrixData))$"transition matrices"
+  #extracting matrices:
+  temp<- extract_mat(MatrixData)
+  surv_mat<-temp$"survival matrices"
+  fert_mat<-temp$"fertility matrices"
+  trans_mat<-temp$"transition matrices"
   
   #calculating the number of stages in the matrix:
   nstage<-1:dim(surv_mat)[1]
+ 
   
-  N0_data<-gen_N0(trans_mat)
+  #Running simulations
+  return_pv<-replicate(10000, calc_pv(surv_mat, fert_mat, trans_mat, N0_data, nstage, years=1:10, stage2mod=active_stages, beta=0.01, active_stages = active_stages))
   
-  return_pv<-replicate(10000, calc_pv(surv_mat, fert_mat, N0_data, nstage, years=1:10, stage2mod=1:3, beta=0.01))
+  #Analysis
+  delta=0.001
+  alpha=seq(from=0.01, to=0.2, by=delta)
+  output<-analyze_pv(alpha, return_pv)
   
-  delta<-0.001
-  alpha<-seq(from=0.01, to=0.2, by=delta)
-  
-  
-  survival_pv<-unlist(return_pv[1,])
-  abundance_pv<-unlist(return_pv[2,])
-  lambda_pv<-unlist(return_pv[3,])
-  
-  prop_demog<-pv_lessthan_alpha(pvalue=survival_pv, alpha)
-  prop_N<-pv_lessthan_alpha(pvalue=abundance_pv,alpha)
-  prop_lambda<-pv_lessthan_alpha(pvalue=lambda_pv, alpha)
-  
-  diff<-(prop_lambda-prop_demog)
-  
-  plot(prop_demog~alpha, type='l',col='blue', xlab='alpha', ylab='freq p<alpha', main=pop_list[k,], xlim=c(0,0.2), ylim=c(0,1))
-  lines(prop_lambda~alpha, col='red')
-  legend("bottomright", c("Survival p-values", "Lambda p-values"), col=c("blue","red"), lty=c(1,1))
-  
-  
-  plot(survival_pv~lambda_pv, col=c("blue", "red"), main='Survival p-value vs. Lambda p-value')
-  abline(0,1)
+  #Visualizing the data
+
+  plot(output$prop_demog~alpha, type='l',col='blue', xlab='alpha', ylab='freq p<alpha', main=poplist[k,], xlim=c(0,0.2), ylim=c(0,1))
+  lines(output$prop_lambda~alpha, col='red')
   legend("topleft", c("Survival p-values", "Lambda p-values"), col=c("blue","red"), lty=c(1,1))
   
-  hist(diff, main='difference of lambda_pv minus survival_pv at beta=0.01')
   
-  print(pop_list[k,])
+  plot(output$survival_pv~output$lambda_pv, main='Survival p-value vs. Lambda p-value', xlab='lambda p-values', ylab='survival p-values')
+  abline(0,1)
+  
+  hist(output$lambda_pv-output$survival_pv, main='Difference of lambda_pv minus survival_pv at beta=0.01')
+  hist(output$lambda_pv, main='Histogram of lambda p-values')
+  hist(output$survival_pv, main='Histogram of survival p-values')
   
 }
+
