719e495bc6e7f909e8a21b231cbbfd2c089ab67b
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-07-16 18:07:30 -0700

    testing

--- analyses/multi_species.R
+++ analyses/multi_species.R
@@ -5,7 +5,7 @@ mydata <- Load_Compadre_Data(CompadreFile)
 
 pop_list <- unique(cbind(mydata$metadata$SpeciesAuthor,mydata$metadata$Population))
 num_pops <- dim(pop_list)[1]
-num_pops <- 5 # for debugging
+num_pops <- 2 # for debugging
 delta=0.001
 alpha=seq(from=0.01, to=0.2, by=delta)
 
@@ -13,11 +13,12 @@ output <- list(NULL)
 
 for (k in 1:num_pops) {
   temp<-subset(mydata$metadata, SpeciesAuthor==pop_list[k,1] & Population==pop_list[k,2])
-  #extracting Matrices
-  save<-as.numeric(rownames(temp))
+  
   MatrixData<- as.array(mydata$mat[mydata$metadata$SpeciesAuthor==pop_list[k,1] & mydata$metadata$Population==pop_list[k,2]])
+  
+  save<-as.numeric(rownames(temp))
   MatClass<-mydata$mat_class[save]
-
+  
   active_stages<-subset(MatClass[[1]]$MatrixClassNumber, MatClass[[1]]$MatrixClassOrganized=='active')
 
   #extracting matrices:
@@ -26,12 +27,13 @@ for (k in 1:num_pops) {
   fert_mat<-temp$"fertility matrices"
   trans_mat<-temp$"transition matrices"
   
+  
   #calculating the number of stages in the matrix:
   nstage<-1:dim(surv_mat)[1]
  
   
   #Running simulations
-  return_pv<-replicate(10000, calc_pv(surv_mat, fert_mat, trans_mat, N0_data, nstage, years=1:10, stage2mod=active_stages, beta=0.01, active_stages = active_stages))
+  return_pv<-replicate(100, calc_pv(surv_mat, fert_mat, trans_mat, fert_zero, N0_data, nstage, years=1:10, stage2mod=active_stages, beta=0.01, active_stages = active_stages))
   
   #Analysis
   output[[k]]<-analyze_pv(alpha, return_pv)
@@ -52,12 +54,12 @@ for(i in 1:length(names)){
 }
 
 #plot to pdf of individual files each
-#for (k in 1:num_pops){
-#
+# for (k in 1:num_pops){
+# 
 #  pdf(paste("plot", names[k], ".pdf", sep=" "), height=20)
 #  plots[[k]]<-plot_pv(output=output[[k]], alpha)
 #  dev.off()
-#}
+# }
 
 
 #plot into single pdf:
