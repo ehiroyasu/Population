e1a0e57d764a9893c748ec25720cd9bc96b787d0
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-07-16 18:07:12 -0700

    created a function to test for zero fertility matrices. DOES NOT WORK, in progress.

---
+++ R/test_fert.R
@@ -0,0 +1,25 @@
+#'test_fert
+#'
+#'Tests for fertility matrices of zero
+#'
+#'@param fert_mat
+#'
+#'@references COMPADRE Plant Matrix Database. Max Planck Institute for Demographic Research (Germany). Available at www.compadre-db.org (data downloaded on [1/1/2014]).
+#'@author Elizabeth Hiroyasu
+#'
+
+test_fert<-function(fert_mat){
+  for (i in 1:dim(fert_mat)[3]){
+    test[i]<-which(fert_mat[,,i]==matrix(0, nrow=dim(fert_mat)[1], ncol=dim(fert_mat)[2]))
+    #test_zero<-which(test=="TRUE")
+    
+  }
+}
+
+
+
+
+
+
+
+apply(fert_mat, 3, which)
