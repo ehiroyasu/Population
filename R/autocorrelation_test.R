e5d5b36a04838033b43ce88a9ac3c3c920bce2af
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-07-21 12:11:08 -0700

    function that looks at autocorrelation in linear models

---
+++ R/autocorrelation_test.R
@@ -0,0 +1,25 @@
+#'autocorrelation_test
+#'
+#'Examines autocorrelation in the regression models from the DemographicTrend package
+#'
+#'@param mod is the linear model, for example, this can be the lm_surv, lm_lambda, or lm_abundance
+#'
+#'@author Elizabeth Hiroyasu
+#'
+#'
+#'
+
+
+
+
+autocorrelation_test<-function(mod){
+
+  res<-mod$residuals
+  autocorrelation_plot<-acf(res)
+  n=length(res)
+  lm_res<- lm(res[-n]~res[-1])
+  res_plot<-plot(abundance_res[-n]~abundance_res[-1])
+  
+  return(list(res, autocorrelation_plot, lm_res, res_plot))
+  
+}
