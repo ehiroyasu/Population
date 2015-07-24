3d5a42b31666fdd9fea079a761182b4a5e927080
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-07-24 13:59:17 -0700

    extracted other explanatory values into output

--- R/analyze_pv.R
+++ R/analyze_pv.R
@@ -13,11 +13,27 @@ analyze_pv<-function(alpha, return_pv){
   
   survival_pv<-unlist(return_pv[1,])
   abundance_pv<-unlist(return_pv[2,])
-  lambda_pv<-unlist(return_pv[3,])
+  log_abundance_pv<-unlist(return_pv[3,])
+  lambda_pv<-unlist(return_pv[4,])
   
   prop_demog<-pv_lessthan_alpha(pvalue=survival_pv, alpha)
   prop_N<-pv_lessthan_alpha(pvalue=abundance_pv,alpha)
+  prop_log_N<-pv_lessthan_alpha(pvalue=log_abundance_pv, alpha)
   prop_lambda<-pv_lessthan_alpha(pvalue=lambda_pv, alpha)
+  
+  surv_zero<-unlist(return_pv[5,])
+  surv_na<-unlist(return_pv[6,])
+  surv_var<-unlist(return_pv[7,])
+  abundance_var<-unlist(return_pv[8,])
+  lambda_var<-unlist(return_pv[9,])
+  mean_lambda<-unlist(return_pv[10,])
+  
+  acf1_abundance<-unlist(return_pv[11,])
+  acf1_lambda<-unlist(return_pv[12,])
 
-  return(list('survival_pv'=survival_pv, 'abundance_pv'=abundance_pv, 'lambda_pv'=lambda_pv, 'prop_demog'=prop_demog, 'prop_N'=prop_N, 'prop_lambda'=prop_lambda))
+  
+  return(list('survival_pv'=survival_pv, 'abundance_pv'=abundance_pv, 'log_abundance_pv'=log_abundance_pv,
+              'lambda_pv'=lambda_pv, 'prop_demog'=prop_demog, 'prop_N'=prop_N, 'prop_log_N'=prop_log_N,
+              'prop_lambda'=prop_lambda, 'surv_zero'=surv_zero, 'surv_na'=surv_na, 'surv_var'=surv_var, 'abundance_var'=abundance_var, 
+              'lambda_var'=lambda_var, 'mean_lambda'=mean_lambda, 'acf1_abundance'=acf1_abundance, 'acf1_lambda'=acf1_lambda))
 }
