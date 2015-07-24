fa94bc471ab24357e0861bee791f6ca13db329c2
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-07-24 13:59:49 -0700

    added autocorrelation testing

--- R/calc_pv.R
+++ R/calc_pv.R
@@ -34,6 +34,7 @@ calc_pv<-function(surv_mat, fert_mat, trans_mat, N0_data, nstage, years, stage2m
     print(abundance)
     plot(abundance[,active_stages])
   }
+ 
   
   ##survival regression
   lm_surv<- surv_regr(surv_trend, active_stages, abundance)
@@ -43,6 +44,10 @@ calc_pv<-function(surv_mat, fert_mat, trans_mat, N0_data, nstage, years, stage2m
   ##abundance regression
   lm_abundance<- abundance_regr(abundance[-1,], years, active_stages)
   pv_abundance<- (lm_abundance$coefficients[3,4])
+  
+  ##log abundance regression
+  lm_log_abundance<-log_abundance_regr(abundance[-1,], years, active_stages)
+  pv_log_abundance<-(lm_log_abundance$coefficients[3,4])
   
   #calculating lambda:
   N_active <- abundance[,active_stages]
@@ -65,9 +70,16 @@ calc_pv<-function(surv_mat, fert_mat, trans_mat, N0_data, nstage, years, stage2m
   surv_var<-var(resid(lm_surv))
   abundance_var<-var(resid(lm_abundance))
   lambda_var<-var(resid(lm_lambda))
+
+  #testing for autocorrelation
+  autocorrelation_abundance<-autocorrelation_test(mod=lm_abundance)
+  acf1_abundance<-autocorrelation_abundance[[2]]$acf[2]
+  
+  autocorrelation_lambda<-autocorrelation_test(mod=lm_lambda)
+  acf1_lambda<-autocorrelation_lambda[[2]]$acf[2]
   
   
-  return(list("survival p-value"=pv_surv, "abundance p-value"=pv_abundance, "lambda p-value" = pv_lambda, 
+  return(list("survival p-value"=pv_surv, "abundance p-value"=pv_abundance, "log abundance p-value"=pv_log_abundance, "lambda p-value" = pv_lambda, 
               "n surv=0"=surv_zero, "n surv=na"=surv_na, "survival variance"=surv_var, "abundance variance" = abundance_var, 
-              "lambda variance"=lambda_var, "mean_lambda"=mean_lambda))
+              "lambda variance"=lambda_var, "mean_lambda"=mean_lambda, "acf1_abundance"=acf1_abundance, "acf1_lambda"=acf1_lambda))
 }
