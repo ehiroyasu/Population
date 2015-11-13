cf577d1c16183b3f255cae5220b6d83f20818bc1
Elizabeth <ehiroyasu@bren.ucsb.edu>
2015-11-13 13:03:58 -0800

    adjusted references

--- R/calc_abundance.R
+++ R/calc_abundance.R
@@ -11,12 +11,6 @@
 #'@references COMPADRE Plant Matrix Database. Max Planck Institute for 
 #'Demographic Research (Germany). Available at www.compadre-db.org 
 #'(data downloaded on [1/1/2014]).
-#'@references Martha M. Ellis, Jennifer L. Williams, Peter Lesica, Timothy J. Bell, Paulette Bierzychudek, Marlin Bowles, 
-#'  Elizabeth E. Crone, Daniel F. Doak, Johan Ehrlen, Albertine Ellis-Adam, Kathryn McEachern, Rengaian Ganesan, 
-#'  Penelope Latham, Sheila Luijten, Thomas N. Kaye, Tiffany M. Knight, Eric S. Menges, William F. Morris, 
-#'  Hans den Nijs, Gerard Oostermeijer, Pedro F. Quintana-Ascencio, J. Stephen Shelly, Amanda Stanley, Andrea Thorpe, 
-#'  Tamara Ticktin, Teresa Valverde, and Carl Weekley. 2012. Matrix population models from 20 studies of perennial 
-#'  plant populations. Ecology 93:951.
 #'
 #'@author Elizabeth Hiroyasu
 
